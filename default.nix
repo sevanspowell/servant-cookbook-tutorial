671a5bda5f1dba31f593b59d5b7482e598442927
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-05-06 22:46:47 +1000

    Get servant-auth-server building

--- default.nix
+++ default.nix
@@ -2,9 +2,14 @@
 let
   inherit (nixpkgs) pkgs;
 
-  haskellPackages = if compiler == "default"
-                    then pkgs.haskellPackages
-                    else pkgs.haskell.packages.${compiler};
+  pkgsTmp = if compiler == "default"
+         then pkgs.haskellPackages
+         else pkgs.haskell.packages.${compiler};
+  haskellPackages = pkgsTmp.extend (self: super: {
+    #jose = self.callHackage "jose" "0.5.0.0" {};
+    servant-auth-server = self.callHackage "servant-auth-server" "0.3.1.0" {};
+  });
+
 in
   {
     servant-cookbook = haskellPackages.callPackage ./servant-cookbook.nix {};
